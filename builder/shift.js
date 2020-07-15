1f660ee0aed01cab22d8f75f957a0e39e1d6c034
Roger Lipscombe <roger@differentpla.net>
2020-07-15 12:36:22 +0100

    Explain why I implemented compile-time bitshift

--- builder/shift.js
+++ builder/shift.js
@@ -1,3 +1,8 @@
+// Squirrel has bitshift operators; see https://developer.electricimp.com/squirrel/squirrel-guide/operators#squirrel-operator-symbols
+// But they're evaluated at runtime, which is slow. This allows you to do constant bitshifts at compile time, and have them readable.
+// Yes; I guess I could have used hex constants for the readability, but if you look at the code where this is used, you can see
+// where I *nearly* got to hiding the whole bit behind a builder macro, so having the indexes and shifts visible kinda makes sense.
+
 module.exports = {
     bsl: (x, n) => x << n
 }
